2b4124766c58066a5aa59401375a96f1fd4fa675
Omurbek Dulatov <oma.dulatov@gmail.com>
2024-04-22 20:59:52 +0600

    domino pizza parsing and show in bot down

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-from rim_pizza import get_rim_pizza
+from parse_domino_pizza import get_pizza_list
 
 
 token = '7194759653:AAHIBWsVCWJX6ydlVEiguv0DkDPPjSHMtGw'
@@ -13,7 +13,7 @@ def start(message):
     bot.reply_to(message, 'Domino приветствует вас!')
     
     murkup = types.ReplyKeyboardMarkup(row_width=2)
-    btn1 = types.KeyboardButton('Риская пицца')
+    btn1 = types.KeyboardButton('Римская пицца')
     btn2 = types.KeyboardButton('Mod пицца')
     btn3 = types.KeyboardButton('Сэндвичи')
     btn4 = types.KeyboardButton('Десерты')
@@ -22,20 +22,49 @@ def start(message):
     bot.send_message(message.chat.id, 'Выберите категорию', reply_markup=murkup)
 
 
+def send_bot_message(chat_id, pizza_list):
+    for pizza in pizza_list:
+        text = f'`Название`: _{pizza["name"]}_\n' \
+               f'`Описание`: _{pizza["description"]}_\n' \
+               f'`Цена`: _{pizza["price"]}_\n' \
+               f'[----------------------------------------]({pizza["picture"]})'
+        keyboard = types.InlineKeyboardMarkup()
+        button_order = types.InlineKeyboardButton(
+            text='Заказать', callback_data=f'order_{pizza["id"]}'
+        )
+        keyboard.add(button_order)
+        bot.send_message(chat_id=chat_id, 
+                         text=text, 
+                         parse_mode='Markdown', 
+                         reply_markup=keyboard)
+
+
+product_categories = {
+    'Римская пицца': 'rimskaya',
+    'Mod пицца': 'moodpizza',
+    'Сэндвичи': 'sendvitchi',
+    'Десерты': 'deserti',
+}
+
+
 @bot.message_handler(func=lambda x: True)
 def echo_all(message):
-    if message.text == 'Риская пицца':
-        pizza_rim_list = get_rim_pizza()
+    pizza_id = product_categories.get(message.text)
+    pizza_list = get_pizza_list(pizza_id=pizza_id)
+    send_bot_message(message.chat.id, pizza_list)
 
-        for pizza in pizza_rim_list:
-            text = f'Название: {pizza["name"]}\n' \
-                   f'Описание: {pizza["description"]}\n' \
-                   f'Цена: {pizza["price"]}'
-            bot.send_photo(
-                message.chat.id, 
-                f'https://static.dodomino.ru/images/3024-1311-picca-s-zapecenoi-goviadinoi-i-gorcicnym-sousom-300.webp'
-            )
-            bot.send_message(message.chat.id, text)
+
+def save_order(from_user, product_info):
+    content = f'Новый заказ от {from_user}:\n\n{product_info}\n\n'
+
+    with open('orders.txt', '+a') as f:
+        f.write(content)
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('order_'))
+def order_pizza(call):
+    from_user = f'{call.message.chat.first_name} {call.message.chat.last_name}'
+    product_info = call.message.text
+    save_order(from_user=from_user, product_info=product_info)
 
 
 bot.polling()
--- parse_domino_pizza.py
+++ parse_domino_pizza.py
@@ -2,19 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_rim_pizza():
+def get_pizza_list(pizza_id=None):
     url = 'https://dominopizza.ru/'
     response = requests.get(url=url, verify=False)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    div_rim = soup.find('div', {'id': 'rimskaya'})
+    div_rim = soup.find('div', {'id': pizza_id})
     div_cls_cols = div_rim.find_all('div', {'class': 'col'})
     pizza_list = []
 
     for div_cls_col in div_cls_cols:
+        div_cls_picture = div_cls_col.a.div.find('div', {'class': 'product-picture'})
+        
         pizza_detail = div_cls_col.a.div.find(
             'div', {'class': 'product-card-content'}
         )
+
+        pizza_id = pizza_detail.find(
+            'div', {'class': 'product-name'}
+        ).get('id')
 
         pizza_name = pizza_detail.find(
             'div', {'class': 'product-name'}
@@ -29,9 +35,11 @@ def get_rim_pizza():
         ).get_text()
 
         pizza_list.append({
+            'id': pizza_id,
             'name': pizza_name,
             'description': pizza_description,
             'price': pizza_price,
+            'picture': div_cls_picture.picture.img.get('src')
         })
 
     return pizza_list
